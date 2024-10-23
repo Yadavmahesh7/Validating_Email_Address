a3e4d04613169da97be43a4800ed09d31553b5a4
Yadavmahesh7 <mahi224224@gmail.com>
2024-10-23 18:57:59 +0530

    EmailValidator file commited

---
+++ EmailValidator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class EmailValidator {
+    public static void main(String[] args) {
+       
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Email");
+        String email=sc.next();
+        String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$";
+
+        Pattern pattern = Pattern.compile(emailRegex);
+        Matcher matcher = pattern.matcher(email);
+
+        if (matcher.matches()) {
+            System.out.println("Valid email address");
+        } else {
+            System.out.println("Invalid email address");
+        }
+    }
+}
